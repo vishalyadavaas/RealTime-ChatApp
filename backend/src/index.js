41c2f14d16663c50414564663db0ca90b445781b
Vishal Yadav <vishalyadavaas@gmail.com>
2025-01-17 03:30:39 +0530

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -34,10 +34,10 @@ app.use("/api/messages", messageRoutes);
 connectDB();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
   });
 }
 
