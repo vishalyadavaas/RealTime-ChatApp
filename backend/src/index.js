458af180da64ffd3d8c6aa04824007824c9ca34d
Vishal Yadav <vishalyadavaas@gmail.com>
2025-01-17 03:13:59 +0530

    route bug fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,47 +1,46 @@
-import express from 'express';
-import { app, server } from './lib/socket.js';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import authRoutes from './routes/auth.route.js';
-import messageRoutes from './routes/message.route.js';
+import express from "express";
+import { app, server } from "./lib/socket.js";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 dotenv.config();
-import connectDB from './lib/database.js'
+import connectDB from "./lib/database.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT;
 
-app.use(express.json({ limit: '5mb' }));
-app.use(express.urlencoded({ limit: '5mb', extended: true }));
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ limit: "5mb", extended: true }));
 app.use(cookieParser());
 
 // cors
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-app.use('/api/auth', authRoutes);
-app.use('/api/messages', messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
-
+// mongodb connection
 connectDB();
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend", "dist', 'index.html'));
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend", "dist', "index.html"));
+  });
 }
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
